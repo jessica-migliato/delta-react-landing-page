6af476c7cdcbd851ab00f28fd8cf8397f8dc64f1
jessica-migliato <jessicalourdes129@gmail.com>
2022-06-01 10:42:40 -0300

    feat: project setup

---
+++ src/data/NavbarData.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    to: "/",
+    text: "About",
+    id: "about",
+  },
+  {
+    to: "/pricing",
+    text: "Pricing",
+    id: "Pricing",
+  },
+  {
+    to: "/signup",
+    text: "SignUp",
+    id: "Contact",
+  },
+];
